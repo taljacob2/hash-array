4c73ff6fdf9c153d9b8adf1de85745584b283d6b
Tal Jacob <taljacob2@gmail.com>
2022-12-02 16:13:44 +0200

    Edit Doc `findIndexOfKey`
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- Hash.h
+++ Hash.h
@@ -82,7 +82,7 @@ int *createHashArray(const int sourceArraySize, int *hashArraySizeOutputParam) {
  *        function returns the next available index in the array, where
  *        `hashArray[index] == HASH_INIT_VALUE`.
  *
- * Runs in O(1/2). That means O(1).
+ * Runs in O(1/2) in the average case. That means O(1) in the average case.
  *
  * @param key
  * @param hashArray
