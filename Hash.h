ad28107d9f8e8dfda4698d10bea11611b10516f7
Tal Jacob <taljacob2@gmail.com>
2022-12-02 15:47:42 +0200

    Add Doc To `findIndexOfKey`
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- Hash.h
+++ Hash.h
@@ -51,6 +51,19 @@ static int getHash(const int key, const int hashArraySize) {
     return getMADHash(1, key, 1, hashArraySize);
 }
 
+/**
+ * @brief Gets the index where the given `key` is presented in the `hashArray`.
+ *        In case the `key` is not present in the `hashArray`, then this
+ *        function returns the next available index in the array, where
+ *        `hashArray[index] == HASH_INIT_VALUE`.
+ *
+ * Runs in O(1/2). That means O(1).
+ *
+ * @param key
+ * @param hashArray
+ * @param hashArraySize
+ * @return
+ */
 int findIndexOfKey(const int key, const int *hashArray,
                    const int hashArraySize) {
     int hash = key;
